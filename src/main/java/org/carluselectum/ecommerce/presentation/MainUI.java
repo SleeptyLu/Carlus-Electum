d7f8a0e0e68d98fabef7e1a4a11173f39ef81273
João Franco <149946525+JoaoFrancoo@users.noreply.github.com>
2025-12-30 21:06:07 +0000

    feat(CEL-01): implemented maven layered structure fixes #1
    I created the Java layers and the structure for the application to start and print a message. As requested in fix number 1.

---
+++ src/main/java/org/carluselectum/ecommerce/presentation/MainUI.java
@@ -0,0 +1,7 @@
+package org.carluselectum.ecommerce.presentation;
+
+public class MainUI {
+    public static void main(String[] args) {
+        System.out.println("Carlus Electum System Started");
+    }
+}
---
+++ src/main/java/org/carluselectum/ecommerce/service/OrderService.java
@@ -0,0 +1,7 @@
+package org.carluselectum.ecommerce.service;
+
+public class OrderService {
+    public void processOrder() {
+        // Business logic
+    }
+}
