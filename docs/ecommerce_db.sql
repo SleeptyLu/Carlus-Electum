40a8063e8bc38cf5841f1b76c4aa3eafbb4bdbc2
João Franco <149946525+JoaoFrancoo@users.noreply.github.com>
2026-01-02 22:46:45 +0000

    fix/jof/updating the database

--- docs/ecommerce_db.sql
+++ docs/ecommerce_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 01, 2026 at 09:09 PM
+-- Generation Time: Jan 02, 2026 at 11:45 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -40,9 +40,28 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`id`, `name`, `price`, `stock`) VALUES
 ('1', 'Monitor Gaming 33\"', 150.00, 6),
-('2', 'Teclado Mecanico RGB', 80.00, 6),
-('3', 'Rato Wireless', 45.00, 7),
+('2', 'Teclado Mecanico RGB', 80.00, 4),
+('3', 'Rato Wireless', 45.00, 4),
 ('4', 'tapete de rato', 20.00, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `email` varchar(100) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`email`, `password`, `role`) VALUES
+('joao@gmail.com', '$2a$10$DlLvHt/jUevJdn1xeZx2g.sWv25Um3JD/NudfVMRHpy3AAnVmEeYO', 'CLIENT');
 
 --
 -- Indexes for dumped tables
